0bdacbf8ff9283b04c3f9de429bad0876d938cbd
JEANS <enrique_sistema_5000@hotmail.com>
2021-08-11 18:34:10 -0500

    agregando archivo composer

--- model/ConexionBD.class.php
+++ model/ConexionBD.class.php
@@ -1,12 +1,18 @@
 <?php
 class ConexionBD{
 	public static function CadenaCN(){
-		return array(
+		/* return array(
 			"servidor" => "localhost",
 			"usuario" => "cevicher_richard",
 			"password" => "vmf-1w~MY#U@",
 			"basedatos" => "cevicher_richardsbd",
+		); */
+
+		return array(
+			"servidor" => "localhost",
+			"usuario" => "root",
+			"password" => "",
+			"basedatos" => "cevicheriarichards",
 		);
 	}
 }
-?>
